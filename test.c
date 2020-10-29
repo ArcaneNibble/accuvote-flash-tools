e16241548057f5950784a27b3931181573761388
R. Ou <rqou@robertou.com>
2020-10-28 22:39:09 -0700

    sleep

--- test.c
+++ test.c
@@ -1,3 +1,11 @@
+#define OSMR0 (*(volatile unsigned int *)0x40A00000)
+#define OSMR1 (*(volatile unsigned int *)0x40A00004)
+#define OSMR2 (*(volatile unsigned int *)0x40A00008)
+#define OSMR3 (*(volatile unsigned int *)0x40A0000C)
+#define OSCR (*(volatile unsigned int *)0x40A00010)
+#define OSSR (*(volatile unsigned int *)0x40A00014)
+#define OIER (*(volatile unsigned int *)0x40A0001C)
+
 int debugbuf_idx;
 char debugbuf[4096];
 
@@ -21,9 +29,21 @@ void debug_8(unsigned char x) {
     debugbuf[debugbuf_idx++] = hexlut[(x >> 0) & 0xF];
 }
 
+void msleep(unsigned int ms) {
+    OSSR = 1;   // clear match
+    OSMR0 = OSCR + 3687 * ms;
+    while ((OSSR & 1) == 0) {}
+}
+
 void entry() {
+    // Setup timer
+    OIER = 1;
+
     debug_str("Hello World! ");
     debug_32(0xcafef00d);
+
+    msleep(30000);
+
     debug_str(" ");
     debug_8(0xa5);
     debug_str(" OwO");
