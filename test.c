215513a151196efadee406e19bf2cb43802aac4d
R. Ou <rqou@robertou.com>
2020-10-29 01:54:24 -0700

    sd init works

--- test.c
+++ test.c
@@ -68,6 +68,14 @@ void debug_32(unsigned int x) {
     }
 }
 
+void debug_16(unsigned short x) {
+    for (int i = 0; i < 4; i++) {
+        if (debugbuf_idx == sizeof(debugbuf))
+            debugbuf_idx = 0;
+        debugbuf[debugbuf_idx++] = hexlut[(x >> ((3 - i) * 4)) & 0xF];
+    }
+}
+
 void debug_8(unsigned char x) {
     if (debugbuf_idx == sizeof(debugbuf))
         debugbuf_idx = 0;
@@ -114,12 +122,10 @@ int mmc_finish_r1(unsigned char cmd, unsigned int *out) {
     if (MMC_STAT & (1 << 1)) {
         mmc_stop_clk();
         return 0;
-    }
-    else if (MMC_STAT & (1 << 5)) {
+    } else if (MMC_STAT & (1 << 5)) {
         mmc_stop_clk();
         return 0;
-    }
-    else {
+    } else {
         unsigned int ret;
         unsigned int tmp = MMC_RES & 0xFFFF;
         if (tmp >> 8 != cmd)
@@ -133,6 +139,62 @@ int mmc_finish_r1(unsigned char cmd, unsigned int *out) {
         ret |= tmp >> 8;
 
         *out = ret;
+
+        mmc_stop_clk();
+        return 1;
+    }
+}
+
+int mmc_finish_r2(unsigned int *out) {
+    if (MMC_STAT & (1 << 1)) {
+        mmc_stop_clk();
+        return 0;
+    } else if (MMC_STAT & (1 << 5)) {
+        mmc_stop_clk();
+        return 0;
+    } else {
+        unsigned int ret;
+        unsigned int tmp = MMC_RES & 0xFFFF;
+        if (tmp >> 8 != 0x3F)
+            return 0;
+        ret = (tmp & 0xFF) << 24;
+
+        tmp = MMC_RES & 0xFFFF;
+        ret |= tmp << 8;
+
+        tmp = MMC_RES & 0xFFFF;
+        ret |= tmp >> 8;
+        
+        out[0] = ret;
+
+        ret = (tmp & 0xFF) << 24;
+
+        tmp = MMC_RES & 0xFFFF;
+        ret |= tmp << 8;
+
+        tmp = MMC_RES & 0xFFFF;
+        ret |= tmp >> 8;
+
+        out[1] = ret;
+
+        ret = (tmp & 0xFF) << 24;
+
+        tmp = MMC_RES & 0xFFFF;
+        ret |= tmp << 8;
+
+        tmp = MMC_RES & 0xFFFF;
+        ret |= tmp >> 8;
+
+        out[2] = ret;
+
+        ret = (tmp & 0xFF) << 24;
+
+        tmp = MMC_RES & 0xFFFF;
+        ret |= tmp << 8;
+
+        // CRC cannot be read
+        
+        out[3] = ret;
 
         mmc_stop_clk();
         return 1;
@@ -193,14 +255,18 @@ void entry() {
     unsigned int ifcond;
     int ret = mmc_finish_r1(8, &ifcond);
     debug_str("ifcond ");
+    int should_try_sdhc = 1;
     if (!ret) {
         debug_str("err ");
+        should_try_sdhc = 0;
     } else {
         debug_32(ifcond);
         debug_str(" ");
     }
 
     unsigned int tries = 100;
+    int init_ok = 0;
+    int is_sdhc = 0;
     while(tries--) {
         mmc_do_cmd(55, 0, 0, 0, 1);
         unsigned int app_cmd;
@@ -216,7 +282,7 @@ void entry() {
 
 
         // SEND_OP_COND
-        mmc_do_cmd(41, 0x40300000, 0, 0, 3);
+        mmc_do_cmd(41, should_try_sdhc ? 0x40300000 : 0x00300000, 0, 0, 3);
         msleep(10);
         unsigned int opcond;
         ret = mmc_finish_r3(&opcond);
@@ -232,9 +298,98 @@ void entry() {
 
         if (opcond & 0x80000000) {
             debug_str("acmd41_ok! ");
+            init_ok = 1;
+            is_sdhc = opcond & 0x40000000;
             break;
         }
     }
+
+    if (!init_ok) {
+        while (1) {}
+    }
+
+    // ALL_SEND_CID
+    mmc_do_cmd(2, 0, 0, 0, 2);
+    unsigned int cid[4];
+    ret = mmc_finish_r2(cid);
+    debug_str("cid ");
+    if (!ret) {
+        debug_str("err ");
+    } else {
+        debug_32(cid[0]);
+        debug_32(cid[1]);
+        debug_32(cid[2]);
+        debug_32(cid[3]);
+        debug_str(" ");
+    }
+
+    // SEND_RELATIVE_ADDR
+    mmc_do_cmd(3, 0, 0, 0, 1);
+    unsigned int rca;
+    ret = mmc_finish_r1(3, &rca);
+    debug_str("rca ");
+    if (!ret) {
+        debug_str("err ");
+        while (1) {}
+    } else {
+        debug_32(rca);
+        debug_str(" ");
+    }
+    rca &= 0xFFFF0000;
+
+    // SEND_CSD
+    mmc_do_cmd(9, rca, 0, 0, 2);
+    unsigned int csd[4];
+    ret = mmc_finish_r2(csd);
+    debug_str("csd ");
+    if (!ret) {
+        debug_str("err ");
+    } else {
+        debug_32(csd[0]);
+        debug_32(csd[1]);
+        debug_32(csd[2]);
+        debug_32(csd[3]);
+        debug_str(" ");
+    }
+
+    // SELECT_CARD
+    mmc_do_cmd(7, rca, 0, 1, 1);
+    unsigned int status;
+    ret = mmc_finish_r1(7, &status);
+    debug_str("select ");
+    if (!ret) {
+        debug_str("err ");
+        while (1) {}
+    } else {
+        debug_32(status);
+        debug_str(" ");
+    }
+
+    // SET_BLOCKLEN
+    mmc_do_cmd(16, 512, 0, 0, 1);
+    ret = mmc_finish_r1(16, &status);
+    debug_str("blocklen ");
+    if (!ret) {
+        debug_str("err ");
+    } else {
+        debug_32(status);
+        debug_str(" ");
+    }
+
+    while ((status & 0x1F00) != 0x900) {
+        // SEND_STATUS
+        mmc_do_cmd(13, rca, 0, 0, 1);
+        ret = mmc_finish_r1(13, &status);
+        debug_str("status ");
+        if (!ret) {
+            debug_str("err ");
+        } else {
+            debug_32(status);
+            debug_str(" ");
+        }
+    }
+
+    debug_str("sd_init_all_ok! ");
 
     while (1) {}
 }
