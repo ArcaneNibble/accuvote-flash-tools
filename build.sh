0592c8655281984aa2b8af7a619dbed59f9f1206
R. Ou <rqou@robertou.com>
2020-10-30 04:23:14 -0700

    gc sections

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 set -xeuo pipefail
 
 arm-none-eabi-gcc -Wall -g -o entry.o -c test.S
-arm-none-eabi-gcc -Wall -std=c11 -g -O2 -c ff.c
-arm-none-eabi-gcc -Wall -std=c11 -g -O0 -c test.c
-arm-none-eabi-gcc -Wall -g -o hax.elf -Wl,-Tlinkscript.ld -nostartfiles -nostdlib test.o entry.o ff.o -lgcc
+arm-none-eabi-gcc -Wall -std=c11 -g -O2 -ffunction-sections -fdata-sections -c ff.c
+arm-none-eabi-gcc -Wall -std=c11 -g -O0 -ffunction-sections -fdata-sections -c test.c
+arm-none-eabi-gcc -Wall -g -o hax.elf -Wl,-Tlinkscript.ld -Wl,--gc-sections -nostartfiles -nostdlib test.o entry.o ff.o -lgcc
