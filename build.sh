13ae95dc14e85aa9b77479f2eaa7e02e621cb894
R. Ou <rqou@robertou.com>
2020-10-30 04:33:36 -0700

    O2 on main code too

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@ set -xeuo pipefail
 
 arm-none-eabi-gcc -Wall -g -o entry.o -c test.S
 arm-none-eabi-gcc -Wall -std=c11 -g -O2 -ffunction-sections -fdata-sections -c ff.c
-arm-none-eabi-gcc -Wall -std=c11 -g -O0 -ffunction-sections -fdata-sections -c test.c
-arm-none-eabi-gcc -Wall -g -o hax.elf -Wl,-Tlinkscript.ld -Wl,--gc-sections -nostartfiles -nostdlib test.o entry.o ff.o -lgcc
+arm-none-eabi-gcc -Wall -std=c11 -g -O2 -ffunction-sections -fdata-sections -c test.c
+arm-none-eabi-gcc -Wall -g -O2 -o hax.elf -Wl,-Tlinkscript.ld -Wl,--gc-sections -nostartfiles -nostdlib test.o entry.o ff.o -lgcc
